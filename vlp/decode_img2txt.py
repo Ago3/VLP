c1fb3340ad6962fcb0ab7113dbf217f046f2d397
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2019-11-12 09:11:10 +0100

    Set code for coco, not babelpic

--- vlp/decode_img2txt.py
+++ vlp/decode_img2txt.py
@@ -265,7 +265,8 @@ def main(parser=None):
         predictions = [{'image_id': tup[1], 'caption': output_lines[img_idx]} for img_idx, tup in enumerate(input_lines)]
 
         print(predictions)
-        # lang_stats = language_eval(args.dataset, predictions, args.model_recover_path.split('/')[-2]+'-'+args.split+'-'+args.model_recover_path.split('/')[-1].split('.')[-2], args.split)
+        # Comment the following line for caption generation on BabelPic
+        lang_stats = language_eval(args.dataset, predictions, args.model_recover_path.split('/')[-2]+'-'+args.split+'-'+args.model_recover_path.split('/')[-1].split('.')[-2], args.split)
 
 
 if __name__ == "__main__":
