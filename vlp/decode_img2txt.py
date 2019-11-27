3697a75bc0d2501eb33494d21e36214a8ce73dd4
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2019-11-27 09:29:29 +0100

    Check output

--- vlp/decode_img2txt.py
+++ vlp/decode_img2txt.py
@@ -258,6 +258,7 @@ def main(parser=None):
                             output_tokens.append(t)
                         output_sequence = ' '.join(detokenize(output_tokens))
                         output_lines[buf_id[i]] = output_sequence
+                        print(input_lines)
                         print(i, output_sequence)
 
                 pbar.update(1)
