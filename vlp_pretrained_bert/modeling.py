91c0fc894e024de3e8a9d0fceb78223e8566bfa7
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2019-12-18 16:42:22 +0100

    Use last 4 layers

--- vlp_pretrained_bert/modeling.py
+++ vlp_pretrained_bert/modeling.py
@@ -1041,9 +1041,7 @@ class BertForPreTrainingLossMask(PreTrainedBertModel):
 
             # vqa2_embed = sequence_output[:, 0]*sequence_output[:, self.len_vis_input+1]
             vqa2_embed = sequence_output[-1][:, 0]*sequence_output[-1][:, self.len_vis_input+1]
-            vqa2_sensembed = torch.stack([v[:, 0] * v[:, self.len_vis_input + 1] for v in sequence_output[-4:]])
-
-            print(vqa2_sensembed.shape)
+            vqa2_sensembed = torch.mean(torch.stack([v[:, 0] * v[:, self.len_vis_input + 1] for v in sequence_output[-4:]]), dim=0)
 
             vqa2_pred = self.ans_classifier(vqa2_embed)
 
