72c131fbfe2b4b0c13e1bc85aca86dd05d16bbdd
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2019-12-18 15:50:28 +0100

    Concat CLS SEP

--- babelpic_preprocessing/sense_embeddings.py
+++ babelpic_preprocessing/sense_embeddings.py
@@ -13,21 +13,41 @@ def generate_vlp_input_file(infile, outfile):
                 synset2n[synset] = 0
             if synset2n[synset] < 5:
                 i += 1
-                synset2n[synset] += 1
                 img_dict = dict()
+                img_dict['synset'] = synset
+                img_dict['offset'] = synset2n[synset]
                 img_dict['question_id'] = i
                 img_dict['image_name'] = line.split('\t')[2]
                 img_dict['feature_path'] = '_feats.pkl'
                 img_dict['question_str'] = '{}: {}'.format(' '.join(line.split('\t')[0].split('_')), line.split('\t')[4])
                 img_dict['has_answer'] = True
                 img_dict['answers'] = [label2ans(line.split('\t')[3])]
+                synset2n[synset] += 1
                 content.append(img_dict)
         print('Instances: ', len(content), ' Concepts: ', len(synset2n))
         np.save(outfile + '_vqa_sensemb.npy', content, allow_pickle=True)
 
 
+def readable_neighbours(dataset, neigh_file, hr_file):
+    synset2lemma = dict()
+    synset2gloss = dict()
+    with open(dataset, 'r') as f:
+        for line in f.readlines():
+            synset = line.split('\t')[1]
+            if synset not in synset2lemma:
+                synset2lemma[synset] = ''.join(line.split('\t')[0].split('_'))
+                synset2gloss[synset] = line.split('\t')[4]
+    with open(neigh_file, 'r') as f, open(hr_file, 'w+') as w:
+        for line in f.readlines():
+            synsets = line.split()
+            for i, synset in enumerate(synsets):
+                offset = '' if i == 0 else '\t'
+                w.write('{}{}\t{}\t{}\n'.format(offset, synset, synset2lemma[synset], synset2gloss[synset]))
+
+
 def main():
-    generate_vlp_input_file('../Image2Synset/DATA/true_examples_15.txt', 'babelpic_gold')
+    # generate_vlp_input_file('../Image2Synset/DATA/true_examples_15.txt', 'babelpic_gold')
+    readable_neighbours('../Image2Synset/DATA/true_examples_15.txt', 'neighbours.tsv', 'neighbours_hr.tsv')
 
 
 if __name__ == '__main__':
--- vlp_pretrained_bert/modeling.py
+++ vlp_pretrained_bert/modeling.py
@@ -1040,6 +1040,8 @@ class BertForPreTrainingLossMask(PreTrainedBertModel):
                 attention_mask, output_all_encoded_layers=False, len_vis_input=self.len_vis_input)
 
             vqa2_embed = sequence_output[:, 0]*sequence_output[:, self.len_vis_input+1]
+            vqa2_sensembed = torch.cat([sequence_output[:, 0], sequence_output[:, self.len_vis_input+1]], dim=1)
+            print(vqa2_sensembed.shape)
             vqa2_pred = self.ans_classifier(vqa2_embed)
 
             # My changes:
@@ -1047,7 +1049,7 @@ class BertForPreTrainingLossMask(PreTrainedBertModel):
             # ans_idx = torch.max(vqa2_pred[:, 1:], -1)[1] + 1
             ans_idx = torch.max(vqa2_pred[:, binary_answer_ids], -1)[1]
 
-            return binary_answer_ids[ans_idx], vqa2_embed
+            return binary_answer_ids[ans_idx], vqa2_sensembed
             #return ans_idx
 
         # zero out vis_masked_pos
