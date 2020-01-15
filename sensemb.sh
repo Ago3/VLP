3d44cf7f4a3c263348c2061dd819674dc91f6cab
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2020-01-15 11:39:45 +0100

    Fix script

--- sensemb.sh
+++ sensemb.sh
@@ -7,7 +7,7 @@ do
 	    --new_segment_ids --enable_butd --image_root /media/SHARED/HDD1_2TB/acalabrese/detectron_data/silver/ \
 	    --src_file /media/SHARED/HDD1_2TB/acalabrese/vlp_data/silver_0_first_half_vqa_sensemb.npy --batch_size 25 \
 	    --region_det_file_prefix feat_cls_1000/silver/ \
-	    --region_bbox_file feat_cls_1000/silver/batch_second_half_bboxes.pkl \
+	    --region_bbox_file feat_cls_1000/silver/batch_0_second_half_bboxes.pkl \
 	    --file_valid_jpgs /media/SHARED/HDD1_2TB/acalabrese/vlp_data/COCO/annotations/coco_valid_jpgs.json --split test --output_dir /media/SHARED/HDD1_2TB/acalabrese/vlp_data/output/silver/0_${half}h --sensemb
 done
 
