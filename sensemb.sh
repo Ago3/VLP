58a974df6c0a7434c6172430c8bc5122357ac9d4
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2020-01-15 08:42:51 +0100

    add script for sensembedding generation

---
+++ sensemb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for batch in 1 2 3 4
+do
+	CUDA_VISIBLE_DEVICES=0 python3 main.py \
+	    --model_recover_path /media/SHARED/HDD1_2TB/acalabrese/vlp_checkpoint/cc_g8_lr1e-4_batch512_s0.75_b0.25/model.30.bin \
+	    --new_segment_ids --enable_butd --image_root /media/SHARED/HDD1_2TB/acalabrese/detectron_data/silver/ \
+	    --src_file /media/SHARED/HDD1_2TB/acalabrese/vlp_data/silver_${batch}_vqa_sensemb.npy --batch_size 25 \
+	    --region_det_file_prefix feat_cls_1000/silver/ \
+	    --region_bbox_file feat_cls_1000/silver/batch_${batch}_bboxes.pkl \
+	    --file_valid_jpgs /media/SHARED/HDD1_2TB/acalabrese/vlp_data/COCO/annotations/coco_valid_jpgs.json --split test --output_dir /media/SHARED/HDD1_2TB/acalabrese/vlp_data/output/silver/${batch} --sensemb
+done
