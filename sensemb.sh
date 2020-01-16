3e30728d7d40f50d336406bc903d38bc5dbc36d3
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2020-01-16 15:24:54 +0100

    Add gold to script

--- sensemb.sh
+++ sensemb.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+CUDA_VISIBLE_DEVICES=0 python3 main.py \
+    --model_recover_path /media/SHARED/HDD1_2TB/acalabrese/vlp_checkpoint/cc_g8_lr1e-4_batch512_s0.75_b0.25/model.30.bin \
+    --new_segment_ids --enable_butd --image_root /media/SHARED/HDD1_2TB/acalabrese/detectron_data/babelpic/ \
+    --src_file /media/SHARED/HDD1_2TB/acalabrese/vlp_data/babelpic_gold_vqa_sensemb.npy --batch_size 25 \
+    --region_det_file_prefix feat_cls_1000/babelpic/ \
+    --region_bbox_file feat_cls_1000/babelpic/all_bboxes.pkl \
+    --file_valid_jpgs /media/SHARED/HDD1_2TB/acalabrese/vlp_data/COCO/annotations/coco_valid_jpgs.json --split test --output_dir /media/SHARED/HDD1_2TB/acalabrese/vlp_data/output --sensemb
+
 for half in "f" "s"
 do
 	CUDA_VISIBLE_DEVICES=0 python3 main.py \
