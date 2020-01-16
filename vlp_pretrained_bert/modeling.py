44f0da3ffe043ffaef71e134cfc825a5d52c84dd
Ago3 <agostina.calabrese3@gmail.com>
2020-01-16 16:11:18 +0100

    w_100ts_4l

--- vlp_pretrained_bert/modeling.py
+++ vlp_pretrained_bert/modeling.py
@@ -1044,14 +1044,21 @@ class BertForPreTrainingLossMask(PreTrainedBertModel):
             vqa2_embed = sequence_output[-1][:, 0]*sequence_output[-1][:, self.len_vis_input+1]
             #vqa2_sensembed = torch.mean(torch.stack([v[:, 0] * v[:, self.len_vis_input + 1] for v in sequence_output[-4:]]), dim=0) 4l
             effective_length = input_ids.shape[1] - (input_ids == 0).sum(dim=1)
-            vqa2_sensembed = torch.stack(sequence_output, dim=0)[-4:].sum(2).sum(0) / effective_length.view(-1, 1).float()  # ts_4l
+            #vqa2_sensembed = torch.stack(sequence_output, dim=0)[-4:].sum(2).sum(0) / effective_length.view(-1, 1).float()  # ts_4l
             #vqa2_sensembed = torch.sum(sequence_output, dim=1) / effective_length.unsqueeze(dim=1).repeat(1, 768).float()  # ts
 
-            attention_probs = self.bert.encoder.layer.attention.self.attention_probs
-            print(attention_probs.shape)
+            attention_probs = torch.stack([v.attention.self.attention_probs for v in self.bert.encoder.layer[-4:]], dim=0)
+            #boolean_softmax_mask = torch.arange(sequence_output[0].shape[1], device=attention_probs.device).view(1, -1).repeat(input_ids.shape[0], 1) >= effective_length.view(-1, 1)
+            #softmax_mask = boolean_softmax_mask.float().masked_fill_(boolean_softmax_mask, float("-inf"))
+            #attention_weights = torch.nn.functional.softmax(attention_probs.sum(2).sum(2).mean(0) + softmax_mask, dim=1)
+            
+            attention_weights = torch.nn.functional.softmax(attention_probs.sum(2).sum(2).mean(0)[:, 1:101], dim=1)
+            bsz, ts, hidden = sequence_output[0].size()
+            vqa2_sensembed = (
+                    (torch.stack(sequence_output[-4:], dim=0).sum(0)[:, 1:101, :] * 
+                    attention_weights.unsqueeze(-1)).sum(1))  # w_ts_4l
 
             vqa2_pred = self.ans_classifier(vqa2_embed)
-
             # My changes:
             binary_answer_ids = torch.tensor([1840, 3117], device=input_ids.device)
             # ans_idx = torch.max(vqa2_pred[:, 1:], -1)[1] + 1
